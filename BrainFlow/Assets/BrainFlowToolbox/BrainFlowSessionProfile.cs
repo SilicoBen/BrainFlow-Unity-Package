9ed6fdecea930b89b17aa11c85aed7e5036bf260
kbnealy <k.b.nealy@gmail.com>
2021-09-01 10:29:49 -0400

    Added Unity Project with BrainFlow integration

---
+++ BrainFlow/Assets/BrainFlowToolbox/BrainFlowSessionProfile.cs
@@ -0,0 +1,12 @@
+﻿using brainflow;
+using UnityEngine;
+
+namespace BrainFlowToolbox
+{
+    [CreateAssetMenu(fileName = "BrainFlowSessionProfile", menuName = "BrainFlow/BrainFlowSessionProfile", order = 0)]
+    public class BrainFlowSessionProfile : ScriptableObject
+    {
+        public BoardIds board;
+        public string boardDataFileName;
+    }
+}
